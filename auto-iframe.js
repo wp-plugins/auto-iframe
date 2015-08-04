086cc996a6bea8e265d67e0e088d5993109dcccb
GregRoss <GregRoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-04 20:51:08 +0000

    - Make sure to check the frame element exists before using it later.
    git-svn-id: https://plugins.svn.wordpress.org/auto-iframe/trunk@1212921 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- auto-iframe.js
+++ auto-iframe.js
@@ -1,6 +1,9 @@
 function AutoiFrameAdjustiFrameHeight( id, fudge )
       {
       var frame = document.getElementById( id );
+	  
+	  if( frame == null ) { return; }
+	  
       var content = jQuery('.embed-wrap');
       var height = frame.contentDocument.body.offsetHeight + fudge;
 
