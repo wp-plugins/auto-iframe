851d7ce489fc9b7839cdf35f682a5ef8f7a8c8a1
GregRoss <GregRoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-05 17:41:25 +0000

    - Make sure to check the frame element and sub-objects exist before using them later.
    git-svn-id: https://plugins.svn.wordpress.org/auto-iframe/trunk@1213630 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- auto-iframe.js
+++ auto-iframe.js
@@ -3,6 +3,8 @@ function AutoiFrameAdjustiFrameHeight( id, fudge )
       var frame = document.getElementById( id );
 	  
 	  if( frame == null ) { return; }
+	  if( frame.contentDocument == null ) { return; }
+	  if( frame.contentDocument.body == null ) { return; }
 	  
       var content = jQuery('.embed-wrap');
       var height = frame.contentDocument.body.offsetHeight + fudge;
